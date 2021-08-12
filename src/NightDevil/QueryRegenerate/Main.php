fb3fb01b5a82b53b7ddadcf5a22ccafb742365b3
DeadBush <66917109+deadbushmc@users.noreply.github.com>
2021-08-12 19:59:35 +0530

    Add files via upload

---
+++ src/NightDevil/QueryRegenerate/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NightDevil\QueryRegenerate;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\QueryRegenerateEvent;
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+    public function onQuery(QueryRegenerateEvent $event): void
+    {
+        $event->setMaxPlayerCount($event->getPlayerCount() + 1);
+        $event->setPlayerCount($event->getPlayerCount() + 0);
+    }
